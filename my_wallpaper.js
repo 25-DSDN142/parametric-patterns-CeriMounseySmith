8cc0655effb02f4c2bb6dbd8d6f97ae85cc8cf6e
CeriMounseySmith <cerimounseysmith@gmail.com>
2025-07-22 11:49:56 +1200

    Changing shapes to circles

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -6,9 +6,9 @@ let centerX = 100;
 let centerY = 100;
 let watermelonX = 120;
 let watermelonY = 130;
-let darkgreenSize = 100;
-let lightgreenSize = 90;
-let pinkSize = 80;
+let darkgreenSize = 120;
+let lightgreenSize = 80;
+let pinkSize = 50;
 let bigseedSize = 10;
 
 //let darkGreen = 21,114,65
@@ -36,31 +36,31 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 //rect(40 ,40, rect_width, rect_height);
 
 //watermelon body ////ORIGINAL/////
-strokeWeight(1.5)
-fill(21,114,65);//dark green
-ellipse(centerX+20,centerY+30,darkgreenSize);
+//strokeWeight(1.5)
+//fill(21,114,65);//dark green
+//ellipse(centerX,centerY,darkgreenSize);
 
-strokeWeight(0);
-fill(117,184,85);//light green
-ellipse(watermelonX,watermelonY,lightgreenSize);
+//strokeWeight(0);
+// fill(117,184,85);//light green
+// ellipse(watermelonX-20,watermelonY-30,lightgreenSize);
 
-strokeWeight(0);
-fill(219,97,97);//pink
-ellipse(watermelonX,watermelonY,pinkSize);
+// strokeWeight(0);
+// fill(219,97,97);//pink
+// ellipse(watermelonX-20,watermelonY-30,pinkSize);
 
 
-//watermelon bigger
-strokeWeight(1.5)
-fill(21,114,65);//dark green
-ellipse(centerX+20,centerY+30,darkgreenSize+50);
+// //watermelon bigger
+// strokeWeight(1.5)
+// fill(21,114,65);//dark green
+// ellipse(centerX+20,centerY+30,darkgreenSize+50);
 
-strokeWeight(0);
-fill(117,184,85);//light green
-ellipse(watermelonX,watermelonY,lightgreenSize);
+// strokeWeight(0);
+// fill(117,184,85);//light green
+// ellipse(watermelonX,watermelonY,lightgreenSize);
 
-strokeWeight(0);
-fill(219,97,97);//pink
-ellipse(watermelonX,watermelonY,pinkSize-40);
+// strokeWeight(0);
+// fill(219,97,97);//pink
+// ellipse(watermelonX,watermelonY,pinkSize-40);
 
 
 
@@ -73,8 +73,8 @@ ellipse(watermelonX,watermelonY,pinkSize-40);
 
 
 // //seeds
-// fill(0,0,0)//blACK
-// ellipse(watermelonX,watermelonY-20,5)//middletop seed
+//fill(0,0,0)//blACK
+//ellipse(watermelonX-20,watermelonY-50,5)//middletop seed
 // fill(0,0,0)
 // beginShape();
 // vertex(117.5,110)
@@ -82,8 +82,8 @@ ellipse(watermelonX,watermelonY,pinkSize-40);
 // vertex(122.5,110)
 // endShape(CLOSE);
 
-// fill(0,0,0)//blACK
-// ellipse(watermelonX,watermelonY+20,5)//middle btm seed ellipse
+ //fill(0,0,0)//blACK
+//ellipse(watermelonX-20,watermelonY+-10,5)//middle btm seed ellipse
 // fill(0,0,0)                         //middle btm seed triangle
 // beginShape();
 // vertex(117.5,150)
@@ -92,8 +92,8 @@ ellipse(watermelonX,watermelonY,pinkSize-40);
 // endShape(CLOSE);
 
 
-// fill(0,0,0)//blACK
-// ellipse(watermelonX+20,watermelonY,5) // right seed ellipse
+//fill(0,0,0)//blACK
+//ellipse(watermelonX,watermelonY-30,5) // right seed ellipse
 // // fill(255)                         //right seed triangle
 // // beginShape();
 // // vertex(117.5,150)
@@ -103,8 +103,29 @@ ellipse(watermelonX,watermelonY,pinkSize-40);
 
 
 
-// fill(0,0,0)//blACK
-// ellipse(watermelonX-20,watermelonY,5) //left seed
+//fill(0,0,0)//blACK
+//ellipse(watermelonX-40,watermelonY-30,5) //left seed
+
+//watermelon body ////new/////
+stroke(117,184,85)
+strokeWeight(3)
+fill(21,114,65);//dark green
+ellipse(centerX,centerY,darkgreenSize);
+
+stroke(219,97,97)
+strokeWeight(3);
+fill(117,184,85);//light green
+ellipse(centerX,centerY,lightgreenSize);
+
+stroke(173,56,56)
+strokeWeight(3);
+fill(219,97,97);//pink
+ellipse(centerX,centerY,pinkSize);
+
+
+
+
+
 
 
 }
