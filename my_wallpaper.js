bd89742868a2f2dcc2f867c03171a3c1c9bae9ee
CeriMounseySmith <cerimounseysmith@gmail.com>
2025-07-29 10:04:59 +1200

    pattern 1

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,14 +1,18 @@
 //your parameter variables go here!
 let centerX = 100;
 let centerY = 100;
-let tlcX = 0 //top left corner x
-let tlcY = 0 //top left corner y 
-let trcX = 200 //top left corner x
-let trcY = 0 //top left corner y
-let blcX = 0 //bottom left corner x
-let blcY = 200 //bottom left corner y
-let brcX = 200 // bottom right corner x
-let brcY = 200 // bottomright corner y
+let tlcX = 0 //top left corner x location
+let tlcY = 0 //top left corner y location
+
+let trcX = 200 //top left corner x location
+let trcY = 0 //top left corner y location
+
+let blcX = 0 //bottom left corner x location
+let blcY = 200 //bottom left corner y location
+
+let brcX = 200 // bottom right corner x location
+let brcY = 200 // bottomright corner y location
+
 let watermelonX = 120;
 let watermelonY = 130;
 let darkgreenSize = 70;
@@ -39,8 +43,8 @@ ellipse(centerX,centerY+15,5,10)//bttm seed
 }
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);   ///can switch between this and one below to see all tiles together
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);   ///can switch between this and one below to see all tiles together
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -84,19 +88,31 @@ fill(219,97,97);
 ellipse(200,200,60,60);
 }
 else{
-fill(219,97,97);
-ellipse(200,200,160,160);//bttm right circle
-fill(0,0,0)
-ellipse(brcX,brcY,10,20)
-ellipse(brcX,brcY,20,10)
+fill(21,114,65)
+ellipse(200,200,180,180)//big dark green circle
 
 fill(219,97,97);
-ellipse(0,0,160,160);//top left circle
+ellipse(200,200,160,160);//big pink circle
 fill(0,0,0)
-ellipse(centerX-100,centerY-50,10,20)
-ellipse(centerX-50,centerY-100,20,10)
+ellipse(brcX,brcY-50,10,20)
+ellipse(brcX-50,brcY,20,10)
+ellipse(brcX,brcY+50,10,20)
+ellipse(brcX+50,brcY,20,10)
+
+// fill(219,97,97);
+// ellipse(0,0,160,160);//top left circle
+// fill(0,0,0)
+// ellipse(centerX-100,centerY-50,10,20)
+// ellipse(centerX-50,centerY-100,20,10)
 
 }
 
 //WaterMelon(20,20,);
 }
+
+
+
+
+
+
+
