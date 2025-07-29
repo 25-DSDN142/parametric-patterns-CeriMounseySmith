a17b04aba641218b74a6481b1d2af34f4d693722
CeriMounseySmith <cerimounseysmith@gmail.com>
2025-07-29 12:05:46 +1200

    All 9 patterns done

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,22 +1,22 @@
 //your parameter variables go here!
-let centerX = 100; //location for watermelon x axis
-let centerY = 100; //location for watermelon y axis
+let centerX = 10; //location for watermelon x axis
+let centerY = 10; //location for watermelon y axis
 
-let brcX = 180;// bottom right corner x location  ///change this to change big pink circle location
-let brcY = 200; // bottom right corner y location
+let brcX = 10;// bottom right corner x location  ///change this to change big pink circle location
+let brcY = 13; // bottom right corner y location
 
 let seedtopX=100; ///*center watermelon seed locations
 let seedtopY=85; ///*
-let seedleftX=85; ///*
+let seedleftX=25; ///*
 let seedleftY=100; ///*
 let seeddownX=100; ///*
 let seeddownY=115; ///*
-let seedrightX=115; ///*
+let seedrightX=165; ///*
 let seedrightY=100; //*
 
-let darkgreenSize = 125; //change this above 80 or under 80 for diff pattern
-let lightgreenSize = 95; //change this to above 90 or below 90 for diff pattern
-let pinkSize = 45; //change this to above or below 50 for diff pattern
+let darkgreenSize = 61; //change this above 80 or under 80 for diff pattern
+let lightgreenSize = 145; //change this to above 90 or below 90 for diff pattern
+let pinkSize = 41; //change this to above or below 50 for diff pattern
 
 function setup_wallpaper(pWallpaper) {
   //pWallpaper.output_mode(DEVELOP_GLYPH);   ///can switch between this and one below to see all tiles together
@@ -28,7 +28,7 @@ function setup_wallpaper(pWallpaper) {
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50; //can change to 0 so they perfectly align like tiles, or 150 to be layed like bricks
+  pWallpaper.grid_settings.row_offset  = 150; //can change to 0 so they perfectly align like tiles, or 150 to be layed like bricks
 }
 
 function wallpaper_background() {
@@ -72,7 +72,7 @@ ellipse(200,200,180,180)//big dark green circle
 fill(219,97,97);
 ellipse(200,200,160,160);//big pink circle
 fill(0,0,0)
-ellipse(brcX,brcY,seedsize1X,seedsize1Y);//seed
+ellipse(brcX,brcY,10,50);//seed
 ellipse(brcX,brcY,50,10);//seed
 ellipse(brcX,brcY,10,50);//seed
 ellipse(brcX,brcY,50,10);//seed
